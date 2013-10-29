ffd5ac3a503c306e0db8dd954eb6afbbffee7236
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 14:19:14 -0500

    second test

--- pizza_spec.rb
+++ pizza_spec.rb
@@ -2,6 +2,21 @@ require 'rspec'
 
 require_relative 'pizza'
 
+describe Pizza do
+  let(:name){"supreme"}
+  let(:description){[starting_topping]}
+  let(:time_baked){0}
+  let(:pizza){Pizza.new(name, description, time_baked)}
+  describe "#initialize" do
+    it "should return a new instance of pizza" do
+      pizza.should be_an_instance_of Pizza
+    end
+    it "throws an argument error when not given arguments" do
+      expect {Pizza.new}.to raise_error(ArgumentError)
+    end
+  end
+end
+
 
 
 
