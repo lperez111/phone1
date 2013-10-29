00f3844fd7ef66dd83a06021e8537c85a8baeba7
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 14:25:23 -0500

    arguments added to initialize methods, also line 7 of test is failing due to syntax

--- pizza.rb
+++ pizza.rb
@@ -1,3 +1,9 @@
 class Pizza
-  
+
+  def initialize(name, description, time_baked)
+    @name = name
+    @description = description
+    @time_baked = time_baked
+  end
+
 end
--- pizza_spec.rb
+++ pizza_spec.rb
@@ -4,11 +4,12 @@ require_relative 'pizza'
 
 describe Pizza do
   let(:name){"supreme"}
-  let(:description){[starting_topping]}
+  let(:description) {[starting_topping]}
   let(:time_baked){0}
   let(:pizza){Pizza.new(name, description, time_baked)}
   describe "#initialize" do
-    it "should return a new instance of pizza" do
+    it "should return a new instance
+     of pizza" do
       pizza.should be_an_instance_of Pizza
     end
     it "throws an argument error when not given arguments" do
@@ -16,6 +17,8 @@ describe Pizza do
     end
   end
 end
+
+
 
 
 
