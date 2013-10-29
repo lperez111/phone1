2d3f88f18a7573c9fc698a6bb5cdb7e8cafdf61a
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 15:54:08 -0500

    toppings added

--- pizza.rb
+++ pizza.rb
@@ -1,9 +1,10 @@
 class Pizza
-  attr_reader :name, :time_baked, :description
+  attr_reader :name, :time_baked, :description, :toppings
   def initialize(name)
     @name = name
     @time_baked = 0
     @description = ""
+    @toppings = []
   end
 
 end
