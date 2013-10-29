f13fae84a033cf8f3d1f49a9b382d9c7978d5860
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-29 14:54:28 -0500

    we changed the number of arguments to accept only name, and we added two new tests on initialize.

--- pizza_spec.rb
+++ pizza_spec.rb
@@ -6,17 +6,25 @@ describe Pizza do
   let(:name){"supreme"}
   let(:description) {""}
   let(:time_baked){0}
-  let(:pizza){Pizza.new(name, description, time_baked)}
+  let(:pizza){Pizza.new(name)}
+  
   describe "#initialize" do
-    it "should return a new instance
-     of pizza" do
+    it "should return a new instance of pizza" do
       pizza.should be_an_instance_of Pizza
     end
     it "throws an argument error when not given arguments" do
       expect {Pizza.new}.to raise_error(ArgumentError)
     end
+    it "should be initialized with a name" do
+      pizza.name.should eq(name)
+    end
+    it "should be initialized with a name string" do
+      pizza.name.should be_a(String)
+    end
   end
 end
+
+
 
 
 
